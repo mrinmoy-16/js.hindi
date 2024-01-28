d615f347a137858eeb5b79d588259057ffa27468
mrinmoy-16 <157892306+mrinmoy-16@users.noreply.github.com>
2024-01-28 05:07:12 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3 + 3)// we are using nodejs,not browser
+
+ let name ="mrinmoy"
+ let age =23
+ let isLoggedIn = true
+
+ // number => 2 to powewr 53
+ // bigint
+ //string =>  ""
+ //boolean = true/false
+ //null =>stanalone value
+ // undefined =>
+ // symbol => unique
+
+
+ // object
+
+ console.log(typeof"null")
+
+
+
+
