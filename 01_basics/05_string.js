97414883c65f91a05d8b8cb74364fea50875c656
mrinmoy-16 <157892306+mrinmoy-16@users.noreply.github.com>
2024-01-30 15:26:57 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "Mrinmoy"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gameName =  new String('Mrinmoymm')
+
+console.log(gameName[0]);
+console.log(gameName);
