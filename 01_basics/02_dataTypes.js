5cd12858a0d71161097641009c16465b5675f094
mrinmoy-16 <157892306+mrinmoy-16@users.noreply.github.com>
2024-01-30 13:27:58 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,24 +1,30 @@
-"use strict";//treat all JS code as newer version
+"use strict"; //treat all JS code as newer version
 
-//alert(3 + 3)// we are using nodejs,not browser
+//alert(3 + 3) // if we are using nodejks, not browser
 
- let name ="mrinmoy"
- let age =23
- let isLoggedIn = true
+console.log(3+3)
 
- // number => 2 to powewr 53
- // bigint
- //string =>  ""
- //boolean = true/false
- //null =>stanalone value
- // undefined =>
- // symbol => unique
+console.log("Mrinmoy")
+
+let name = "Mrinmoy"
+let age = 23
+let isLoggedIn = false
+  
+// THATS ALL ARE PRIMITIVE DATA TYPE//
+
+// number => 2 to power 52
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
 
 
- // object
-
- console.log(typeof"null")
-
+// THATS ALL ARE NON - PRIMITIVE //
+ 
+// object 
+console.log(typeof age);
 
 
 
