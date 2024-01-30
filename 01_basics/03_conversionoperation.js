c2a48e1f11dca65c95a56f9b50d1964e4f9163e3
mrinmoy-16 <157892306+mrinmoy-16@users.noreply.github.com>
2024-01-30 13:49:43 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,10 +1,18 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
 
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber );
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+let isLoggedin = 1
+let booleanISLoggedIn = Boolean(isLoggedIn)
+console.log(boleanIsLoggedIn);
