185b6bab782c52684779f8388d71e6d0d6af5570
mrinmoy-16 <157892306+mrinmoy-16@users.noreply.github.com>
2024-01-31 10:56:48 +0000

    datesinJs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,23 @@
+//DATES
+
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocalString());
+//console.log(typeof myDate);
+
+
+let myCreatedDate = new Date("01-31-2024") 
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Date.now()/1000)
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
